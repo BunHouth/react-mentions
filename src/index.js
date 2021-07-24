d11cfc98c7334cba0033188a25000de2e21b5ed9
Bunhouth <bunhouth99@gmail.com>
2021-07-24 15:40:36 +0900

    expose markupToRegex

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as MentionsInput } from './MentionsInput'
 export { default as Mention } from './Mention'
 export { default as getMentions } from './utils/getMentions';
+export { default as markupToRegex } from './utils/markupToRegex';
